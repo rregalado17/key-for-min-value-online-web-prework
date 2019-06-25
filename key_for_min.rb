4e4fa50fb18907cbed5f46d0f33438b6524f8878
magical-flow-3186 <rregalado17@gmail.com>
2019-06-25 19:03:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_value = 0
+  lowest_value = nil 
   lowest_key = nil
   
   name_hash.each do |key,value|
   #you need to group the keys and values in order to compare them in groups
 
-    if lowest_value == 0 || value < lowest_value
+    if lowest_value == nil || value < lowest_value
       lowest_value = value 
       lowest_key = key
     end
